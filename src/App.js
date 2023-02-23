6d84871a274718e3f67653e8d6e1f5ca35d464ff
Ckai <ckai.trans@gmail.com>
2023-02-23 23:05:00 +0800

    U89 done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import AddUser from './components/Users/AddUser';
+
+import './index.css';
 
 function App() {
+
   return (
     <div>
-
+      <AddUser></AddUser>
     </div>
   );
 }
